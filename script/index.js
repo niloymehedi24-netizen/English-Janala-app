702e5c6614830a695b59c3629aed134bf2057c16
hasanniloy122-rgb <hasanniloy122@gmail.com>
2026-03-06 15:34:20 +0600

    fixed the null and empty words with relevant words

--- script/index.js
+++ script/index.js
@@ -20,22 +20,25 @@ const displayLevelWord = (words) =>{
     const wordContainer = document.getElementById('word-container');
     wordContainer.innerHTML= '';
 
-//    {
-//     "id": 19,
-//     "level": 1,
-//     "word": "Sincere",
-//     "meaning": "সত্‍ / আন্তরিক",
-//     "pronunciation": "সিনসিয়ার"
-// }
+    if(words.length == 0){
+        wordContainer.innerHTML= `
+        <div class="text-center col-span-3 font-bangla rounded-full py-10 space-y-4">
+            <img class="mx-auto" src="./assets/alert-error.png" alt="">
+            <p class="text-sm text-[#79716B]">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="text-2xl text-[#292524] font-medium">নেক্সট Lesson এ যান</h2>
+        </div>
+        `;
+        return;
+    }
 
     words.forEach(word => {
         console.log(word);
         const wordCard = document.createElement('div');
         wordCard.innerHTML = `
          <div class="bg-white rounded-md shadow-sm text-center py-10 px-5 space-y-6">
-            <h2 class="font-bold text-2xl">${word.word}</h2>
+            <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
             <p class="font-medium">Meaning /Pronounciation</p>
-            <div class="font-bangla text-2xl font-medium text-[#18181B]">" ${word.meaning} / ${word.pronunciation}"</div>
+            <div class="font-bangla text-2xl font-medium text-[#18181B]">" ${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "উচ্চারণ পাওয়া যায়নি"}"</div>
             <div class="flex justify-between items-center">
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF]"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF]"><i class="fa-solid fa-volume-high"></i></button>
